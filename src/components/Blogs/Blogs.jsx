66a9a23daebdd67f73e394df2d1765b252c2d22f
asif247 <86229363+asif247@users.noreply.github.com>
2023-04-12 03:47:53 +0600

    first question added

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -5,7 +5,17 @@ const Blogs = () => {
   return (
     <div className=''> 
       <Banner>Blogs</Banner>
-     
+      <div className='px-3 md:px-20 lg:px-52'>
+        <div className="mt-10">
+          <div className="grid grid-cols-1 gap-4 text-justify">
+            <div className="border-solid border-2 p-4 bg-slate-100">
+              <h3 className="text-2xl">1.Difference between map,filter,foreach and find </h3>
+              <p className="mt-2">In JavaScript, map, filter, forEach, and find are all methods that can be called on
+                        arrays to manipulate or iterate over them.</p>
+            </div>
+          </div>
+        </div>
+     </div>
     </div>
   );
 };
