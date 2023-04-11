eac8368574d6b17aae77a717f829adf1ee5873ee
asif247 <86229363+asif247@users.noreply.github.com>
2023-04-12 04:13:39 +0600

    question 2 added

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -9,9 +9,16 @@ const Blogs = () => {
         <div className="mt-10">
           <div className="grid grid-cols-1 gap-4 text-justify">
             <div className="border-solid border-2 p-4 bg-slate-100">
-              <h3 className="text-2xl">1.Difference between map,filter,foreach and find </h3>
-              <p className="mt-2">In JavaScript, map, filter, forEach, and find are all methods that can be called on
-                        arrays to manipulate or iterate over them.</p>
+              <h3 className="text-2xl">1.What is ContextApi ? </h3>
+              <p className="mt-2">The Context API is a React structure that enables you to exchange unique details and assists in solving prop-drilling from all levels of your application.</p>
+            </div>
+          </div>
+        </div>
+        <div className="mt-10">
+          <div className="grid grid-cols-1 gap-4 text-justify">
+            <div className="border-solid border-2 p-4 bg-slate-100">
+              <h3 className="text-2xl">2.What is custom hook in react.js? </h3>
+              <p className="mt-2">Custom React JS hooks are reusable functions that a React JS software developer can use to add special and unique functionality to the React applications. </p>
             </div>
           </div>
         </div>
