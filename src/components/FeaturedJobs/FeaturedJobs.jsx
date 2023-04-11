6bdf939925f6ae70f65486e8d99b08898389b482
asif247 <86229363+asif247@users.noreply.github.com>
2023-04-12 04:27:58 +0600

    done

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -17,9 +17,8 @@ const FeaturedJobs = () => {
           <p className="text-3xl font-semibold pt-8 md:pt-14 pb-4">
             Featured Jobs
           </p>
-          <p className="text-xs">
-            Explore thousands of job opportunities with all the information you
-            need. Its your future
+          <p className="text-xl">
+          The Featured Jobs option can help you find the talent that you need. For a one-time fee of $29.99, you can feature your job post to help reach more freelancers 
           </p>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-3 my-7">
